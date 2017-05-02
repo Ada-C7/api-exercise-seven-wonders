6b1103e369295a8950f41da71367e6f3dac6577c
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-01 22:17:04 -0700

    Google map api homework done

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,15 +1,17 @@
 require 'httparty'
+require 'awesome_print'
 
-#Starter Code:
+def location(place_arry)
+  @url = "https://maps.googleapis.com/maps/api/geocode/json"
+  @place_hash = {}
+
+  place_arry.each do |place|
+    query_params = {"address" => place}
+    response = HTTParty.post(@url,query: query_params)
+    @place_hash[place] = response["results"][0]["geometry"]["location"]
+  end
+  return @place_hash
+end
+
 seven_wonders = ["Great Pyramind of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Lighthouse of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
-
-
-
-
-
-
-
-
-
-#Example Output:
-#{"Great Pyramind of Giza"=>{"lat"=>29.9792345, "lng"=>31.1342019}, "Hanging Gardens of Babylon"=>{"lat"=>32.5422374, "lng"=>44.42103609999999}, "Colossus of Rhodes"=>{"lat"=>36.45106560000001, "lng"=>28.2258333}, "Lighthouse of Alexandria"=>{"lat"=>38.7904054, "lng"=>-77.040581}, "Statue of Zeus at Olympia"=>{"lat"=>37.6379375, "lng"=>21.6302601}, "Temple of Artemis"=>{"lat"=>37.9498715, "lng"=>27.3633807}, "Mausoleum at Halicarnassus"=>{"lat"=>37.038132, "lng"=>27.4243849}}
+print loc = location(seven_wonders)
