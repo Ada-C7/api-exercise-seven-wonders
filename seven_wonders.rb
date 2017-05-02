8fbac0eeefded93da9ffca6620daf2c6057b6539
Emma Claire Philippides <acme9898@gmail.com>
2017-05-01 21:00:08 -0700

    generated hash

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -3,4 +3,13 @@ require 'awesome_print'
 
 seven_wonders = ["Great Pyramind of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Lighthouse of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
 
-BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+
+seven_wonders_hash = {}
+
+seven_wonders.each do |wonder|
+  response = HTTParty.get("#{URL}#{wonder}")
+  seven_wonders_hash[wonder] = response["results"][0]["geometry"]["location"]
+end
+
+  puts seven_wonders_hash
