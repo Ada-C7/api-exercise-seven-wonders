7abe2894d16af1da6299b824408cb2f4deaa8e75
Kayla <kubicke.kayla@gmail.com>
2017-05-01 20:02:09 -0700

    Program returns nested hash with name, latitude, and longitude.

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,9 +1,35 @@
 require 'httparty'
+require 'json'
 
-#Starter Code:
-seven_wonders = ["Great Pyramind of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Lighthouse of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
+BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?"
+
+SEVEN_WONDERS_URL = [
+  "Great+Pyramind+of+Giza",
+  "Hanging+Gardens+of+Babylon",
+  "Colossus+of+Rhodes",
+  "Lighthouse+of+Alexandria",
+  "Statue+of+Zeus+at+Olympia",
+  "Temple+of+Artemis",
+  "Mausoleum+at+Halicarnassus"
+]
+
+test_hash = {}
 
 
+
+SEVEN_WONDERS.each do |one_wonder|
+  url ="#{BASE_URL}address=#{one_wonder}"
+  response = HTTParty.get(url).parsed_response
+
+  latitude =  JSON.parse response["results"][0]["geometry"]["location"]["lat"].to_json
+  longitude = JSON.parse response["results"][0]["geometry"]["location"]["lng"].to_json
+
+   one_wonder_name = one_wonder.tr('+', ' ')
+
+  test_hash.merge!( one_wonder_name =>{"lat" => latitude, "lng" => longitude})
+end
+
+puts JSON.pretty_generate(test_hash)
 
 
 
@@ -12,4 +38,10 @@ seven_wonders = ["Great Pyramind of Giza", "Hanging Gardens of Babylon", "Coloss
 
 
 #Example Output:
-#{"Great Pyramind of Giza"=>{"lat"=>29.9792345, "lng"=>31.1342019}, "Hanging Gardens of Babylon"=>{"lat"=>32.5422374, "lng"=>44.42103609999999}, "Colossus of Rhodes"=>{"lat"=>36.45106560000001, "lng"=>28.2258333}, "Lighthouse of Alexandria"=>{"lat"=>38.7904054, "lng"=>-77.040581}, "Statue of Zeus at Olympia"=>{"lat"=>37.6379375, "lng"=>21.6302601}, "Temple of Artemis"=>{"lat"=>37.9498715, "lng"=>27.3633807}, "Mausoleum at Halicarnassus"=>{"lat"=>37.038132, "lng"=>27.4243849}}
+#{"Great Pyramind of Giza"=>{"lat"=>29.9792345, "lng"=>31.1342019},
+# "Hanging Gardens of Babylon"=>{"lat"=>32.5422374, "lng"=>44.42103609999999},
+# "Colossus of Rhodes"=>{"lat"=>36.45106560000001, "lng"=>28.2258333},
+# "Lighthouse of Alexandria"=>{"lat"=>38.7904054, "lng"=>-77.040581},
+# "Statue of Zeus at Olympia"=>{"lat"=>37.6379375, "lng"=>21.6302601},
+# "Temple of Artemis"=>{"lat"=>37.9498715, "lng"=>27.3633807},
+# "Mausoleum at Halicarnassus"=>{"lat"=>37.038132, "lng"=>27.4243849}}
